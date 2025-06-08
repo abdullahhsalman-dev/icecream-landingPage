b950b3ff84e4ceea7f768c49c0aa01d8ce762b60
abdullahsalman <abdullahh.salman.dev@gmail.com>
2025-06-08 13:23:40 +0500

    updated layout value

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "SweetScoops Ice Cream",
+  description:
+    "Indulge in delicious ice cream flavors at SweetScoops, your go-to shop for sweet treats!",
 };
 
 export default function RootLayout({
